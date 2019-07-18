37dc0470a29aa3181d92d16658bd907e25e81e56
JuganD <40771401+JuganD@users.noreply.github.com>
2019-07-18 22:12:45 +0300

    Update PrivateConfiguration.cs

--- ChronoPlus.Lightweight.Windows/PrivateConfiguration.cs
+++ ChronoPlus.Lightweight.Windows/PrivateConfiguration.cs
@@ -8,6 +8,6 @@ namespace ChronoPlus.Lightweight.Windows
 {
     public static class PrivateConfiguration
     {
-        public const string UpdateUrl = "";
+        public const string UpdateUrl = "https://raw.githubusercontent.com/JuganD/ChronoPlus/master/chronoplus_updateinfo.xml";
     }
 }
