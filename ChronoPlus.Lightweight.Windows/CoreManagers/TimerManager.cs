6c804be37fdc9637617cfab2b7b99c9317cbe730
JuganD <nishtodrugo@gmail.com>
2019-11-07 21:20:22 +0200

    Fully implemented check for shop offers with notifications, that will fire on timed vote and at application startup

--- ChronoPlus.Lightweight.Windows/CoreManagers/TimerManager.cs
+++ ChronoPlus.Lightweight.Windows/CoreManagers/TimerManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using ChronoPlus.Controller.Models;
 
 namespace ChronoPlus.Lightweight.Windows.CoreManagers
@@ -58,14 +59,17 @@ namespace ChronoPlus.Lightweight.Windows.CoreManagers
                                 {
                                     Window.InsertedLabels.Add("    From chest reward: " + chestCoins);
                                 }
-                                // TODO: check for new offers
                             }
                         }
+
+                        CheckOffersAndPromptNotification(controller);
+
                         if (controller.ResponseResult == Result.InvalidToken)
                         {
                             if (IconManager.icon != null)
                                 IconManager.icon.ShowBalloonTip(3000, "Chrono+", "Error! Invalid/expired JWT token!", System.Windows.Forms.ToolTipIcon.Error);
-                        } else if (controller.ResponseResult == Result.Unknown)
+                        }
+                        else if (controller.ResponseResult == Result.Unknown)
                         {
                             if (IconManager.icon != null)
                                 IconManager.icon.ShowBalloonTip(3000, "Chrono+", "Error! Chrono spin not successful, check your internet connection!", System.Windows.Forms.ToolTipIcon.Error);
@@ -89,6 +93,56 @@ namespace ChronoPlus.Lightweight.Windows.CoreManagers
             int msUntilFour = (int)((fiveOClock - now).TotalMilliseconds);
 
             return msUntilFour;
+        }
+
+        // Sends check offers request, finds those offers which are released after the 
+        // last check date and prompts notification if that is the case
+        private void CheckOffersAndPromptNotification(Controller controller)
+        {
+            if (!ConfigManager.Config.ContainsKey("offersLastCheck"))
+            {
+                ConfigManager.Config.Add("offersLastCheck", DateTime.MinValue.ToString());
+            }
+
+            string lastCheckTimeString = ConfigManager.Config["offersLastCheck"];
+            DateTime lastCheckTime = DateTime.Parse(lastCheckTimeString); // this is UTC
+
+            bool lastCheckIsNotToday = (DateTime.UtcNow.Date - lastCheckTime.Date) >= TimeSpan.FromDays(1);
+            if (lastCheckIsNotToday)
+            {
+                var offersModel = controller.CheckOffers();
+                var newOffers = offersModel?.Offers
+                    .Where(x => x.Active == true
+                             && x.SoldOut == false
+                             && x.CreatedOn > lastCheckTime);
+
+                if (newOffers.Any())
+                {
+                    if (IconManager.icon != null)
+                    {
+                        string newOffersMessage = "New offers available!";
+
+                        // Write the name of every offer if they are lower than 5
+                        // (we don't want to write 100 offers for example)
+                        if (newOffers.Count() <= 5) 
+                        {
+                            newOffersMessage += " (" + string.Join(", ", newOffers.Select(x => x.Name)) + ")";
+                        }
+
+                        IconManager.BalloonTipClickedAction = delegate
+                        {
+                            System.Diagnostics.Process.Start("https://www.chrono.gg/shop");
+                        };
+                        IconManager.icon.ShowBalloonTip(5000, "Chrono+",
+                            newOffersMessage,
+                            System.Windows.Forms.ToolTipIcon.Info);
+                        
+                    }
+                }
+            }
+
+            ConfigManager.Config["offersLastCheck"] = DateTime.UtcNow.ToString();
+            new ConfigManager().SaveConfig();
         }
     }
 }
